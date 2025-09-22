0b933f0b98f879c0c5c34c9e6a516c6f1abb0e6b
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-22 14:24:10 -0400

    refactor: mejorar la lógica de carga de la página del capítulo del curso

--- src/app/(root)/(public)/courses/[slug]/[chapterId]/page.tsx
+++ src/app/(root)/(public)/courses/[slug]/[chapterId]/page.tsx
@@ -1,7 +1,30 @@
+import { ChapterPage } from "@/components/pages/chapterPage/chapterPage"
+import { getCourseAndChaptersByPurchase } from "@/lib/api/get-course-and-chapter-by-purchase"
+import { getCourseBySlug } from "@/lib/api/get-course-by-slug"
+import { CourseNotPurchase } from "@/components/pages/courses-page/not-purchase"
+
+export default async function ChapterCoursePage({ params }: {
+    params: Promise<{ slug: string, chapterId: string }>
+}) {
 
 
-export default function ChapterCoursePage() {
+    const { slug, chapterId } = await params
+    const course = await getCourseBySlug(slug)
+    if (!course) {
+        return <div>course not found</div>
+    }
+
+
+    const purchase = await getCourseAndChaptersByPurchase({ courseid: course.id })
+
+    if (!purchase) {
+        return <CourseNotPurchase course={course} slug={slug} chapterId={chapterId} />
+    }
+
+
+
+
     return (
-        <div>page</div>
+        <ChapterPage chapterId={chapterId} slug={slug} purchase={purchase} />
     )
 }
