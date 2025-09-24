ad312bca73db842ae764d3caa9a3caf22e08febc
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-24 18:01:58 -0400

    implement filtering in the api

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -3,15 +3,37 @@ import { createClient } from "@/utils/supabase/server";
 
 
 export async function GET(req:NextRequest) {
+    const {searchParams} = req.nextUrl
+    
+    const category = (searchParams.getAll('category').length > 0 ? searchParams.getAll('category') : ['all'])
+    const level = (searchParams.get('level') || 'all')
+    const search = (searchParams.get('search') || 'all')
+    const page = (searchParams.get('page') || 1)
+    const rating = Number((searchParams.get('rating')) || 0)
+    const limit = Number((searchParams.get('limit')) || 10)
+
+    
+
+    
+    
+    const offset = (Number(page) - 1) * limit
     
     try {
         const supabase = await createClient()
 
  
         
-        const { data, error } = await supabase
-        .rpc('get_courses')
+        const { data, error } = await supabase.rpc("get_courses",{
+            minimum_rating: rating,
+            p_categorys: category,
+            p_levels: [level],
+            p_limit: limit,
+            p_offset: offset
+        })
 
+        console.log(data );
+        
+      
 
         if (error || !data){
             return NextResponse.json({error})
