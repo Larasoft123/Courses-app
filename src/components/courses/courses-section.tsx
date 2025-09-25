95b3c89dd43401c8f3a054c15698e62e5608c7eb
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-25 18:44:57 -0400

    refactor: update to use new api data

--- src/components/courses/courses-section.tsx
+++ src/components/courses/courses-section.tsx
@@ -1,10 +1,18 @@
 import { getCourses } from "@/lib/api/get-courses"
 import { CourseList } from "./courses-list/courses-list"
 
+const category: string[] = []
+const levels: string[] = []
+const page: number = 1
+
+
 
 
 export async function CoursesSection() {
-    const courses = await getCourses({})
+
+
+
+    const { courses } = await getCourses({ category, levels, page })
 
 
 
@@ -15,7 +23,7 @@ export async function CoursesSection() {
         <section id="courses" className="py-20 ">
 
 
-            <div className="container  z-10 mx-auto px-4">
+            <div className="container z-10 mx-auto px-4">
                 <div className="text-center mb-16">
                     <h2 className="text-4xl md:text-5xl font-bold mb-6 bg-gradient-to-r from-foreground to-primary bg-clip-text text-transparent">
                         Featured Courses
@@ -25,8 +33,8 @@ export async function CoursesSection() {
                     </p>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <CourseList courses={courses} />
+                <div>
+                    <CourseList className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mb-8" courses={courses} />
                 </div>
             </div>
         </section>
