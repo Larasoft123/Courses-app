bae7ab55ed468cce64de62af90c7e4ab82c79226
Moises Lara <Moisesdavidlara1@gamil.com>
2025-09-25 18:45:33 -0400

    refactor: get className from props

--- src/components/courses/courses-list/courses-list.tsx
+++ src/components/courses/courses-list/courses-list.tsx
@@ -1,15 +1,18 @@
 import { Course } from "@/types/course-card.types"
 import { CourseCard } from "../card/course-card"
-export function CourseList({ courses }: { courses: Course[] }) {
+export function CourseList({ courses, className }: { courses: Course[], className?: string }) {
 
     return (
-        <ul>
+        <ul className={className} >
             {
-                courses.map((course, index) => (
-                    <li key={course.id}>
-                        <CourseCard course={course} index={index} />
-                    </li>
-                ))
+                courses.map((course, index) => {
+
+
+                    return (
+                        <li key={course.course_id}>
+                            <CourseCard course={course} index={index} />
+                        </li>)
+                })
             }
         </ul>
     )
